d3117d420d318589ca353234f4431f5998464383
E11even <ihind.mizo@gmail.com>
2021-06-17 20:43:44 +0100

    Done the picture in picture

---
+++ script.js
@@ -0,0 +1,21 @@
+const videoElement = document.getElementById("video");
+const button = document.getElementById("button");
+
+// the async/await
+async function selectMediaStream() {
+	try {
+		const mediaStream = await navigator.mediaDevices.getDisplayMedia();
+		videoElement.srcObject = mediaStream;
+		videoElement.onloadedmetadata = () => {
+			videoElement.play();
+		};
+	} catch (error) {
+		console.log("whoops, Error's here:", error);
+	}
+}
+button.addEventListener("click", async () => {
+	button.disabled = true;
+	await videoElement.requestPictureInPicture();
+	button.disabled = false;
+});
+selectMediaStream();
